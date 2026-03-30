b707e7af224a4ba044b35d37c7264c0deb6c619e
AedinMC <aedin.cowan@ontariotechu.net>
2026-03-30 11:27:09 -0400

    basic study app functionality

---
+++ study-app/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+let sessions = [];
+
+app.get('/api/sessions', (req, res) => {
+  res.json(sessions);
+});
+
+app.post('/api/sessions', (req, res) => {
+  const newSession = {
+    id: Date.now(),
+    ...req.body
+  };
+
+  sessions.push(newSession);
+  res.json(newSession);
+});
+
+app.delete('/api/sessions/:id', (req, res) => {
+  const id = Number(req.params.id);
+  sessions = sessions.filter(s => s.id !== id);
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
