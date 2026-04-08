d3f66d56670c1fb54f3ba3d6a234a9f1edc8a46d
JuanTonzs <JuanJoaquinPascual19@gmail.com>
2026-04-08 12:30:36 -0400

    Updated Graphs
    Added line graph that shows duration over date

--- study-app/handle_data.js
+++ study-app/handle_data.js
@@ -50,3 +50,56 @@ function updateStudyChart(data) {
         .call(d3.axisLeft(y));
 
 }
+
+
+function updateLineChart(data) {
+    d3.select("#line-chart-container").selectAll("*").remove();
+
+    if (!data || data.length === 0) {
+        return;
+    }
+
+    const formattedData = data.map(d => ({
+        date: new Date(d.date),
+        value: +d.duration
+    })).sort((a, b) => a.date - b.date);
+
+    // data[0] gives the array for the actual data passed through
+    console.log(data[0]['date']);
+    var date = data[0]['date'];
+
+    var svg = d3.select("#line-chart-container")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+    const x = d3.scaleTime()
+        .domain(d3.extent(formattedData, d => d.date))
+        .range([0, width]);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
+
+   
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(formattedData, d => d.value)])
+      .nice()
+      .range([height, 0]);
+      
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+
+    svg.append("path")
+      .datum(formattedData)
+      .attr("fill", "none")
+      .attr("stroke", "#7f56d9")
+      .attr("stroke-width", 2)
+      .attr("d", d3.line()
+        .x(d => x(d.date))
+        .y(d => y(d.value))
+    );
+}
